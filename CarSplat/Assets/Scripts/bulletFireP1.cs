2d286ba04de22aa36e1406ae0d7095c9e8fba976
Daniel Williams <dtwilli1@uci.edu>
2017-03-01 11:15:59 -0800

    Setup for Game 3

---
+++ CarSplat/Assets/Scripts/bulletFireP1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletFireP1 : MonoBehaviour {
+	public GameObject bullet;
+	public float bulletSpeed = 5.0f;
+	// Use this for initialization
+		
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject thebullet = (GameObject)Instantiate (bullet, transform.position, transform.rotation);
+			thebullet.GetComponent<Rigidbody> ().AddForce (transform.forward * bulletSpeed, ForceMode.Impulse);
+		}
+	}
+}
---
+++ CarSplat/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+	
+	private Rigidbody rb;
+	public float speed = 5.0f;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+	}
+}
